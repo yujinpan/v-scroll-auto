6e95198ba64ea6330771bcb28accb98b22d2f7f8
yujinpan <1192878390@qq.com>
2021-02-22 16:57:29 +0800

    Update example

--- README.md
+++ README.md
@@ -42,17 +42,19 @@ export default {
 ```vue
 <template>
   <div>
+    <label>Default:</label>
     <ul class="cm-list-unstyled cm-padding-medium" v-auto-scroll>
       <li class="cm-margin-top-base" v-for="item in 5" :key="item">
         <el-button>{{ item }}</el-button>
       </li>
     </ul>
+    <label>speed = 5, axis = x:</label>
     <ul
       class="cm-list-unstyled cm-flex cm-padding-large cm-border-base"
       v-auto-scroll:5.x
     >
       <li v-for="item in 10" :key="item">
-        <el-button style="width: 200px;" type="primary">{{ item }} </el-button>
+        <el-button style="width: 200px;" type="primary">{{ item }}</el-button>
       </li>
     </ul>
   </div>
--- examples/App.vue
+++ examples/App.vue
@@ -9,24 +9,23 @@
       </el-col>
       <el-col class="cm-text-left" :span="12">
         <div>
-          <label>Function 1:</label>
-          <div class="cm-overflow-hidden">
-            <ul class="cm-list-unstyled cm-padding-medium" v-auto-scroll>
-              <li class="cm-margin-top-base" v-for="item in 5" :key="item">
-                <el-button>{{ item }}</el-button>
-              </li>
-            </ul>
-            <ul
-              class="cm-list-unstyled cm-flex cm-padding-large cm-border-base"
-              v-auto-scroll:5.x
-            >
-              <li v-for="item in 10" :key="item">
-                <el-button style="width: 200px;" type="primary">{{
-                  item
-                }}</el-button>
-              </li>
-            </ul>
-          </div>
+          <label>Default:</label>
+          <ul class="cm-list-unstyled cm-padding-medium" v-auto-scroll>
+            <li class="cm-margin-top-base" v-for="item in 5" :key="item">
+              <el-button>{{ item }}</el-button>
+            </li>
+          </ul>
+          <label>speed = 5, axis = x:</label>
+          <ul
+            class="cm-list-unstyled cm-flex cm-padding-large cm-border-base"
+            v-auto-scroll:5.x
+          >
+            <li v-for="item in 10" :key="item">
+              <el-button style="width: 200px;" type="primary">{{
+                item
+              }}</el-button>
+            </li>
+          </ul>
         </div>
         <el-divider></el-divider>
         <el-link
@@ -40,7 +39,7 @@
 </template>
 
 <script>
-import { AutoScroll } from '@/index';
+import AutoScroll from '@/index';
 
 const version = require('../package').version;
 
