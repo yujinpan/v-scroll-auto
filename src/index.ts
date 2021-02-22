b31154d34f0e61e66956cbc97f02abbb966c25fb
yujinpan <1192878390@qq.com>
2021-02-22 16:51:02 +0800

    feat: export vue plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export { default as AutoScroll } from './directives/auto-scroll';
+import AutoScroll from './directives/auto-scroll';
+import { VueConstructor } from 'vue';
+
+// @ts-ignore
+AutoScroll.install = (Vue: VueConstructor) => {
+  Vue.directive('auto-scroll', AutoScroll);
+};
+
+export default AutoScroll;
