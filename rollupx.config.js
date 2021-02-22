183ed7d9f109bbed069e1bde6c3ebbcbd658f219
yujinpan <1192878390@qq.com>
2021-02-22 16:49:55 +0800

    Update env config

--- rollupx.config.js
+++ rollupx.config.js
@@ -2,7 +2,7 @@ module.exports = {
   banner:
     '/*!\n' +
     ` * v-auto-scroll v${require('./package.json').version}\n` +
-    ` * (c) 2019-${new Date().getFullYear()} yujinpan\n` +
+    ` * (c) 2021-${new Date().getFullYear()} yujinpan\n` +
     ' * Released under the MIT License.\n' +
     ' */\n'
 };
